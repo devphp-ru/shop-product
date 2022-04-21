66b79e98ebbbcb2923030bb716ad5d8fbf18a105
Alex N <webxak31@yandex.ru>
2022-04-21 22:13:18 +0300

    Инициализация нового проекта v.01

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+error_reporting(-1);
+
+//session_start();
+//$start = microtime(true);
+//printf('Затрачено %.4F сек.', microtime(true) - $start);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
